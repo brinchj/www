1dd4730eaaf73b1998696310be30eacdff08d215
Johan Brinch <zerrez@gmail.com>
2009-04-19 23:23:54 +0200

    must less down time during update

--- push.sh
+++ push.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+WEB_PATH=/vol/www/hjemmesider/studerende/zerrez
+
+
 echo "remove old version.."
 rm -rf ../build/* 
 
@@ -18,8 +23,10 @@ for t in css js png html txt; do
 done
 
 echo "upload new version.."
-ssh brok.diku.dk rm -rf /vol/www/hjemmesider/studerende/zerrez/*
-scp -r ../build/* brok.diku.dk:~/www/
+ssh brok.diku.dk "mkdir ~/www_new"
+scp -r ../build/* brok.diku.dk:~/www_new/
+ssh brok.diku.dk "rm -rfv $WEB_PATH/* && mv ~/www_new/* $WEB_PATH && rmdir ~/www_new"
+
 
 echo "disk usage:"
-ssh brok.diku.dk "du -sh /vol/www/hjemmesider/studerende/zerrez/"
+ssh brok.diku.dk "du -sh $WEB_PATH"
