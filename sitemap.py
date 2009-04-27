65216fc57d9b9d0c935dacfd7188c2a95323b4c9
Johan Brinch <zerrez@gmail.com>
2009-04-27 15:16:07 +0200

    sitemap -> identifies pdfs

--- sitemap.py
+++ sitemap.py
@@ -1,8 +1,19 @@
-import re, os
+import re, os, glob
 from datetime import datetime
 
+def find_pages(dirs):
+    if not dirs: return []
+    pages = []
+    for d in dirs:
+        pages += filter(lambda x: '/_' not in x, glob.glob('%s/*.html' % d))
+        pages += find_pages([ '%s/%s' % (d, p) for p in os.listdir(d) if os.path.isdir(d+'/'+p)])
+    return pages
+
+pages = ['footer.html'] + find_pages(['./sections'])
+
+
 URL = 'http://diku.dk/hjemmesider/studerende/zerrez/'
-RE_LINK = re.compile(r'<a\s+href="http://[^z]+/zerrez/([^"]+)"')
+RE_LINK = re.compile(r'<a\s+href="((http://[^z"]+/zerrez/[^"]+)|(http://zerrez.s3.amazonaws.com/[^"]+))"')
 
 print 'updating sitemap.html ..'
 
@@ -11,16 +22,20 @@ def to_file(link):
     return 'sections/%s.html' % link[1:]
 
 def modified(link):
-    return datetime.fromtimestamp(os.stat(link).st_mtime).isoformat()
+    lastmod = None
+    if 'zerrez/sections' in link:
+        lastmod = datetime.fromtimestamp(os.stat(link[link.index('zerrez/sections/')+7:]).st_mtime).isoformat()
+    return lastmod and 'lastmod=%s' % lastmod or ''
 
 
-links = []
-html  = file('footer.html').read()
-for link in RE_LINK.findall(html):
-    print 'adding:', link
-    links.append((URL + link, modified(link)))
+links = set()
+for p in pages:
+    html  = file(p).read()
+    for link,_,_ in RE_LINK.findall(html):
+        print 'adding:', link
+        links.add((link, modified(link)))
 
 file('sitemap.txt','w').write('\n'.join(
-    [ '%s\tlastmod=%s' % link for link in links]))
+    [ '%s %s' % link for link in links]))
 
 print 'links:', len(links)
