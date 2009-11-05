dc218522f77d30b080cdc52c53750342728320c8
Johan Brinch <zerrez@gmail.com>
2009-11-05 15:04:08 +0100

    Recommendations section and minor fixes in scripts

--- push.sh
+++ push.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/zsh
 
 WEB_PATH=/vol/www/hjemmesider/studerende/zerrez
 
 
 echo ">> remove old version.."
-rm -rf ../build/* 
+rm -rf ../build/*
 
 python sitemap.py
 
 echo ">> copy new version.."
 for t in css js png html txt; do
-    for i in `find . -iname "*.$t"`; do   
+    for i in `find . -iname "*.$t"`; do
         mkdir -p ../build/`dirname $i`;
-        if [ $t == "html" ]; then
+        if [ "$t" = "html" ]; then
             cat header.html >> ../build/$i;
             cat $i >> ../build/$i;
             cat footer.html >> ../build/$i;
@@ -26,10 +26,10 @@ done
 #appcfg.py update --email zerrez@gmail.com --no_cookies gapp || exit -1
 
 echo ">> update zerrez @ diku.."
-ssh brok.diku.dk "mkdir ~/www_new"
-scp -r ../build/* brok.diku.dk:~/www_new/
-ssh brok.diku.dk "rm -rf $WEB_PATH/* && mv ~/www_new/* $WEB_PATH && rmdir ~/www_new"
+ssh zerrez@brok.diku.dk "mkdir ~/www_new"
+scp -r ../build/* zerrez@brok.diku.dk:~/www_new/
+ssh zerrez@brok.diku.dk "rm -rf $WEB_PATH/* && mv ~/www_new/* $WEB_PATH && rmdir ~/www_new"
 
 
 echo ">> disk usage:"
-ssh brok.diku.dk "du -sh $WEB_PATH"
+ssh zerrez@brok.diku.dk "du -sh $WEB_PATH"
